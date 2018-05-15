0a857eaf6d9391b5b59c0fcd36b875845f67da3f
Wu Xinyang <vervain2012@gmail.com>
2018-05-15 20:54:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-import express from 'express';
-import path from 'path';
-import bodyParser from 'body-parser';
+//import express from 'express';
+//import path from 'path';
+//import bodyParser from 'body-parser';
 
+
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
